045299bc210146a176d20cc0b417419aa6903971
Dmitrii.Morozov <snoopdesigns@gmail.com>
2016-12-02 21:12:53 +0300

    countdown implemented

--- build.bat
+++ build.bat
@@ -9,6 +9,7 @@ if not exist %1\target mkdir %1\target
 mkdir %1\target\gamemodes
 mkdir %1\target\scriptfiles
 cd %1
+%2\pawno\pawncc.exe -l %1\gamemodes\awesome-freeroam.pwn
 %2\pawno\pawncc.exe -w200 -w217 -v0 -o%1\target\gamemodes\awesome-freeroam.amx %1\gamemodes\awesome-freeroam.pwn -; -(
 xcopy /s /q /Y %1\scriptfiles %1\target\scriptfiles
 xcopy /s /q /Y %1\target %2\
